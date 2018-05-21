15ae6d823eddae60c090834af5c246a945aec56e
Giulia Campana <giuliaecampana@gmail.com>
2018-05-21 12:31:25 -0700

    Updated so that code doesn't need to iterate through each row, but can just iterate over entire board to add an X or O. Updated so that an X or O won't be placed if there is already an X/O placed in square.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,49 +1,41 @@
+// keeps track of whose turn it is:
 var playerX = true;
 var playerO = false;
 
-// adds an X to a square if clicked:
-var row1 = document.getElementById("row1");
-var row2 = document.getElementById("row2");
-var row3 = document.getElementById("row3");
+// adds a piece to the board:
 
-for (var i = 0; i < row1.children.length; i++) {
-  row1.children[i].addEventListener('click', function(event) {
-  	if (playerX) {
-  	  event.target.innerHTML = 'X';
-  	  playerX = false;
-  	  playerO = true;
-  	} else if (playerO) {
-  	  event.target.innerHTML = 'O';
-  	  playerX = true;
-  	  playerO = false;
-  	}
-  });
+// Before placing an X or O, ensure the clicked board square is empty. If the position is already occupied, don't place an X or O and do not move on to the next player's turn.
+// After each play, look for 3 in a row, either diagonally, horizontally and vertically. Also be sure to check to see if the board is full.
+// If either condition is met, display a message and do not allow any additional plays.
+
+
+var board = document.getElementById("board");
+var rows = board.children[0].children;
+
+for (var i = 0; i < rows.length; i++) {
+
+  for (var j = 0; j < rows[i].children.length; j++) {
+  	var square = rows[i].children[j];
+
+  	square.addEventListener('click', function(event) {
+  	  if (playerX && event.target.innerHTML === '') {
+  	    event.target.innerHTML = 'X';
+  	    playerX = false;
+  	    playerO = true;
+
+  	  } else if (playerO && event.target.innerHTML === '') {
+  	    event.target.innerHTML = 'O';
+  	    playerX = true;
+  	    playerO = false;
+  	  }
+  	});
+  }
 }
 
-for (var j = 0; j < row2.children.length; j++) {
-  row2.children[j].addEventListener('click', function(event) {
-  	if (playerX) {
-  	  event.target.innerHTML = 'X';
-  	  playerX = false;
-  	  playerO = true;
-  	} else if (playerO) {
-  	  event.target.innerHTML = 'O';
-  	  playerX = true;
-  	  playerO = false;
-  	}
-  });
-}
 
-for (var k = 0; k < row3.children.length; k++) {
-  row3.children[k].addEventListener('click', function(event) {
-  	if (playerX) {
-  	  event.target.innerHTML = 'X';
-  	  playerX = false;
-  	  playerO = true;
-  	} else if (playerO) {
-  	  event.target.innerHTML = 'O';
-  	  playerX = true;
-  	  playerO = false;
-  	}
-  });
-}
+
+
+
+
+
+
