1becc9e21d99aad9c8fe5a8a04d3d9a03370602e
Giulia Campana <giuliaecampana@gmail.com>
2018-05-21 12:07:07 -0700

    Updated app.js to implement most recent commit properly

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -11,3 +11,15 @@ for (var i = 0; i < row1.children.length; i++) {
   	event.target.innerHTML = 'X';
   });
 }
+
+for (var j = 0; j < row2.children.length; j++) {
+  row2.children[j].addEventListener('click', function(event) {
+  	event.target.innerHTML = 'X';
+  });
+}
+
+for (var k = 0; k < row3.children.length; k++) {
+  row3.children[k].addEventListener('click', function(event) {
+  	event.target.innerHTML = 'X';
+  });
+}
