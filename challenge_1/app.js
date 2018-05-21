aaff91b72f6df5b9fb604faf10f1f71d3526bb15
Giulia Campana <giuliaecampana@gmail.com>
2018-05-21 12:14:36 -0700

    swaps between two players and changes between X and O placed on the board. player X always starts first.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,25 +1,49 @@
-// for each square on board
-//	add an event listener for a click
-//	click will make an X appear on the square
+var playerX = true;
+var playerO = false;
 
+// adds an X to a square if clicked:
 var row1 = document.getElementById("row1");
 var row2 = document.getElementById("row2");
 var row3 = document.getElementById("row3");
 
 for (var i = 0; i < row1.children.length; i++) {
   row1.children[i].addEventListener('click', function(event) {
-  	event.target.innerHTML = 'X';
+  	if (playerX) {
+  	  event.target.innerHTML = 'X';
+  	  playerX = false;
+  	  playerO = true;
+  	} else if (playerO) {
+  	  event.target.innerHTML = 'O';
+  	  playerX = true;
+  	  playerO = false;
+  	}
   });
 }
 
 for (var j = 0; j < row2.children.length; j++) {
   row2.children[j].addEventListener('click', function(event) {
-  	event.target.innerHTML = 'X';
+  	if (playerX) {
+  	  event.target.innerHTML = 'X';
+  	  playerX = false;
+  	  playerO = true;
+  	} else if (playerO) {
+  	  event.target.innerHTML = 'O';
+  	  playerX = true;
+  	  playerO = false;
+  	}
   });
 }
 
 for (var k = 0; k < row3.children.length; k++) {
   row3.children[k].addEventListener('click', function(event) {
-  	event.target.innerHTML = 'X';
+  	if (playerX) {
+  	  event.target.innerHTML = 'X';
+  	  playerX = false;
+  	  playerO = true;
+  	} else if (playerO) {
+  	  event.target.innerHTML = 'O';
+  	  playerX = true;
+  	  playerO = false;
+  	}
   });
 }
