8e46221c899a263e24a8f185ed7c3faeb261b44d
Giulia Campana <giuliaecampana@gmail.com>
2018-05-21 12:05:03 -0700

    Adds an X to a square if square is clicked

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,13 @@
-console.log('testing hi!');
+// for each square on board
+//	add an event listener for a click
+//	click will make an X appear on the square
 
+var row1 = document.getElementById("row1");
+var row2 = document.getElementById("row2");
+var row3 = document.getElementById("row3");
+
+for (var i = 0; i < row1.children.length; i++) {
+  row1.children[i].addEventListener('click', function(event) {
+  	event.target.innerHTML = 'X';
+  });
+}
