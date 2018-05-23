e28d459a663264f125b4a26cf98c920a979754dd
Giulia Campana <giuliaecampana@gmail.com>
2018-05-22 19:22:45 -0700

    have working server, ajax requests, html, and algorithm for converting JSON to CSV

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,54 @@
+var app = {
+  server: 'http://127.0.0.1:1337',
+
+  onSubmitClick: function() {
+  	// input form:
+  	var text = $("#input").val();
+  	console.log(text);
+	// event listener for submit button:
+    $("#submit").on('click', app.send(text));
+  },
+
+  send: function(JSONdata) {
+	$.ajax({
+	  url: app.server,
+	  type: 'POST',
+	  data: JSONdata,
+	  contentType: 'application/json',
+	  success: function (data) {
+	  	// Clear data from form:
+	    // app.$input.val('');
+	    // $("#data").display = "none";
+	    // $("#submit").display = "none";
+	    console.log('data processed!!!!');
+	    $("output").val(data);
+	  },
+	  error: function (error) {
+	    console.error('error: ', error);
+	  }
+	});
+  }
+
+};
 
 
+var test = {'these': 'hi',
+			'are': 'hello',
+			'my': 'howdy',
+			'keys': [{
+				'these': 'hola',
+				'are': 'hallo',
+				'my': 'bonjour',
+				'keys': []
+			}, {
+				'these': 'ciao',
+				'are': 'olà',
+				'my': 'namaste',
+				'keys': [{
+					'these': 'nihao',
+					'are': 'salaam',
+					'my': 'marhaba',
+					'keys': []
+				}]
+			}]
+		}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,64 @@
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(express.static('client'));
+app.use(bodyParser.json());
+
+var getKeys = function(data) {
+  var keys = [];
+  for (var key in data) {
+  	if (!Array.isArray(data[key])) {
+  	  keys.push(key);
+  	}
+  }
+  return keys;
+};
+
+var convertData = function(data) {
+  var values =[];
+  var eachObjValues =[];
+
+  var keys = getKeys(data);
+  var dataKeys = Object.keys(data);
+
+  for (var i = 0; i < dataKeys.length; i++) {
+  	if (keys[i] === dataKeys[i]) {
+  	  eachObjValues.push(data[dataKeys[i]]);
+
+  	} else {
+  	  for (var j = 0; j < data[dataKeys[i]].length; j++) {
+  	  	eachObjValues.push(convertData(data[dataKeys[i]][j]));
+
+  	  }
+  	}
+  }
+  return eachObjValues.join(',');
+};
+
+var formatData = function(data) {
+  var keys = getKeys(data);
+  var allValues = convertData(data);
+  var valArray = allValues.split(',');
+  var everything = keys.concat(valArray);
+  var values = [];
+
+  for (var k = 0; k < everything.length; k+=6) {
+  	values.push(everything[k] + ',' + everything[k + 1] + ',' + everything[k + 2] + ',' + everything[k + 3] + ',' + everything[k + 4] + ',' + everything[k + 5]);
+  }
+
+  return values.join('\n');
+}
+
+
+app.post('/', function(req, res) {
+  var csv = formatData(req.body);
+  res.send(csv);
+});
+
+http.createServer(app).listen(1337);
+console.log('express server is listening on port 1337');
+
 
 
